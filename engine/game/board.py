e222a3e8397250178df1f6edff85ebbaf02acbb1
Jingjie Yang <j.yang19@ejm.org>
2017-08-17 21:30:43 +0200

    unittest update (won't pass yet)

--- engine/game/board.py
+++ engine/game/board.py
@@ -40,6 +40,7 @@ class Board:
         :return: the Forsyth-Edwards notation of the current board
 
         >>> Board.create_standard_board().to_FEN()
+        rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1
         """
         pass
 
